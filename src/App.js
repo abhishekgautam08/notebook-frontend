d0752f03f8059b2eda39e98bb2cceec4b052e37f
Abhishek <2015pceecabhishek003@poornima.org>
2022-10-21 02:20:16 +0530

    create routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import "./App.css";
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import About from "./components/About";
 function App() {
-  return <div className="App"></div>;
+  return (
+    <>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </Router>
+    </>
+  );
 }
 
 export default App;
